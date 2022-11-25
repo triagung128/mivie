692c2f6895c65718a255dc0653d74066ac516cbb
triagung128 <triagung128@gmail.com>
2022-11-25 15:02:59 +0700

    fix entity movie

--- lib/domain/entities/movie.dart
+++ lib/domain/entities/movie.dart
@@ -22,21 +22,30 @@ class Movie extends Equatable {
     required this.overview,
     required this.posterPath,
     required this.title,
+    this.adult,
+    this.backdropPath,
+    this.genreIds,
+    this.originalTitle,
+    this.popularity,
+    this.releaseDate,
+    this.video,
+    this.voteAverage,
+    this.voteCount,
   });
 
-  bool? adult;
-  String? backdropPath;
-  List<int>? genreIds;
-  int id;
-  String? originalTitle;
-  String? overview;
-  double? popularity;
-  String? posterPath;
-  String? releaseDate;
-  String? title;
-  bool? video;
-  double? voteAverage;
-  int? voteCount;
+  final bool? adult;
+  final String? backdropPath;
+  final List<int>? genreIds;
+  final int id;
+  final String? originalTitle;
+  final String? overview;
+  final double? popularity;
+  final String? posterPath;
+  final String? releaseDate;
+  final String? title;
+  final bool? video;
+  final double? voteAverage;
+  final int? voteCount;
 
   @override
   List<Object?> get props => [
