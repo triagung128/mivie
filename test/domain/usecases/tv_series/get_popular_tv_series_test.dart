71776f343c800bf8a5bb0615000930a05a32c696
triagung128 <triagung128@gmail.com>
2022-11-26 13:21:57 +0700

    added usecases for tv series

---
+++ test/domain/usecases/tv_series/get_popular_tv_series_test.dart
@@ -0,0 +1,29 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/domain/usecases/tv_series/get_popular_tv_series.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import '../../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late MockTvSeriesRepository mockTvSeriesRepository;
+  late GetPopularTvSeries usecase;
+
+  setUp(() {
+    mockTvSeriesRepository = MockTvSeriesRepository();
+    usecase = GetPopularTvSeries(mockTvSeriesRepository);
+  });
+
+  final tTvSeries = <TvSeries>[];
+
+  test('should get list of popular tv series from the repository', () async {
+    // arrange
+    when(mockTvSeriesRepository.getPopularTvSeries())
+        .thenAnswer((_) async => Right(tTvSeries));
+    // act
+    final result = await usecase.execute();
+    // assert
+    expect(result, Right(tTvSeries));
+  });
+}
