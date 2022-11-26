63df66499d73b7f19c3557cd216b8fc076e6399f
triagung128 <triagung128@gmail.com>
2022-11-26 13:16:45 +0700

    added entities for tv series

---
+++ lib/domain/entities/tv_series.dart
@@ -0,0 +1,50 @@
+import 'package:equatable/equatable.dart';
+
+class TvSeries extends Equatable {
+  TvSeries({
+    required this.posterPath,
+    required this.popularity,
+    required this.id,
+    required this.backdropPath,
+    required this.voteAverage,
+    required this.overview,
+    required this.firstAirDate,
+    required this.originCountry,
+    required this.genreIds,
+    required this.originalLanguage,
+    required this.voteCount,
+    required this.name,
+    required this.originalName,
+  });
+
+  final String? posterPath;
+  final double popularity;
+  final int id;
+  final String? backdropPath;
+  final double voteAverage;
+  final String overview;
+  final String firstAirDate;
+  final List<String> originCountry;
+  final List<int> genreIds;
+  final String originalLanguage;
+  final int voteCount;
+  final String name;
+  final String originalName;
+
+  @override
+  List<Object?> get props => [
+        posterPath,
+        popularity,
+        id,
+        backdropPath,
+        voteAverage,
+        overview,
+        firstAirDate,
+        originCountry,
+        genreIds,
+        originalLanguage,
+        voteCount,
+        name,
+        originalName,
+      ];
+}
---
+++ lib/domain/entities/tv_series_detail.dart
@@ -0,0 +1,61 @@
+import 'package:ditonton/domain/entities/genre.dart';
+import 'package:ditonton/domain/entities/season.dart';
+import 'package:equatable/equatable.dart';
+
+class TvSeriesDetail extends Equatable {
+  TvSeriesDetail({
+    required this.backdropPath,
+    required this.firstAirDate,
+    required this.genres,
+    required this.id,
+    required this.lastAirDate,
+    required this.name,
+    required this.numberOfEpisodes,
+    required this.numberOfSeasons,
+    required this.overview,
+    required this.posterPath,
+    required this.seasons,
+    required this.status,
+    required this.tagline,
+    required this.type,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  final String? backdropPath;
+  final String firstAirDate;
+  final List<Genre> genres;
+  final int id;
+  final String lastAirDate;
+  final String name;
+  final int numberOfEpisodes;
+  final int numberOfSeasons;
+  final String overview;
+  final String? posterPath;
+  final List<Season> seasons;
+  final String status;
+  final String tagline;
+  final String type;
+  final double voteAverage;
+  final int voteCount;
+
+  @override
+  List<Object?> get props => [
+        backdropPath,
+        firstAirDate,
+        genres,
+        id,
+        lastAirDate,
+        name,
+        numberOfEpisodes,
+        numberOfSeasons,
+        overview,
+        posterPath,
+        seasons,
+        status,
+        tagline,
+        type,
+        voteAverage,
+        voteCount,
+      ];
+}
